a384736ef84ae7af072c5d682b4de74640f3d543
Farooq <farooq@teqniqly.com>
2021-11-28 10:56:43 -0800

    First

---
+++ AzBatch.Management.Api/Controllers/CreateApplicationPackageResponse.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="CreateApplicationPackageResponse.cs" company="Teqniqly">
+// Copyright (c) Teqniqly. All rights reserved.
+// </copyright>
+
+namespace Teqniqly.AzBatch.Management.Api.Controllers
+{
+    using System;
+
+    public class CreateApplicationPackageResponse
+    {
+        public string StorageUrl { get; set; }
+
+        public DateTime? StorageUrlExpiresOn { get; set; }
+
+        public string ApplicationPackageState { get; set; }
+    }
+}
---
+++ AzBatch.Management.Infrastructure/Extensions.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="Extensions.cs" company="Teqniqly">
+// Copyright (c) Teqniqly. All rights reserved.
+// </copyright>
+
+namespace Teqniqly.AzBatch.Management.Infrastructure
+{
+    using System;
+
+    public static class Extensions
+    {
+        public static bool ApplicationPackagesExist(this BatchManagementServiceException batchManagementServiceException)
+        {
+            return string.Compare(
+                batchManagementServiceException
+                    .NativeCloudException
+                    .Body
+                    .Code,
+                "ApplicationPackagesNotEmpty",
+                StringComparison.OrdinalIgnoreCase) == 0;
+        }
+    }
+}
